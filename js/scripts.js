cd6475e602145017c35068d6c837d4e62fde02bb
David Boedigheimer <stealth259@gmail.com>
2019-12-05 14:15:48 -0800

    Add ids and addTask functions to javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
 // Back end
 function toDoList() {
   this.tasks = [];
+  this.currentId = 0;
 }
 
-function Tasks(newTask) {
+toDoList.prototype.addTask = function(task) {
+  task.id = this.assignId();
+  this.tasks.push(task);
+}
+
+toDoList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+function Task(newTask) {
   this.newTask = newTask;
 }
 
-Tasks.prototype.findTasks = function (id) {
-  for (var i = 0; i < this.tasks.length; i++) {
-    if (this.tasks[i].id == id) {
-      return this.tasks[i];
-    }
-  };
-  return false;
-}
+// toDoList.prototype.deleteTasks = function(id) {
+//   for (var i = 0; i < this.tasks.length; i++) {
+//     if (this.tasks[i]) {
+//       if (this.tasks[i].id == id) {
+//         delete this.tasks[i];
+//         return true;
+//       }
+//     }
+//   };
+//   return false;
+// }
 // Front end
 $(document).ready(function() {
   var user = prompt("Please enter your name", "Name Here");
@@ -23,13 +37,14 @@ $(document).ready(function() {
   } else {
     $("#user").append(user);
   }
+  var newList = new toDoList()
 
   $("form#list").submit(function(e) {
     e.preventDefault();
     var newTask = $("input#work").val();
-
-    var newListItem = new Tasks(newTask);
-
+    var newListItem = new Task(newTask);
+    newList.addTask(newListItem);
+console.log(newList);
     $("ol#tasks").append("<li>" + newListItem.newTask + "</li>");
   })
 
